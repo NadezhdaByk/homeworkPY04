1667f61bed640524827011e0ba5a6f09ef7eec92
Nadezhda <nbykova79@gmail.com>
2022-09-18 10:38:27 +0300

    задача вывода многочлена при вводе степени

--- TASK04.py
+++ TASK04.py
@@ -14,14 +14,14 @@ for i in range(k+1):
 print(cof)
 arr=[]
 utf={0: "\u2070", 1: "\u00B9",2: "\u00B2",3: "\u00B3", 4: "\u2074", 5: "\u2075",
-6: "\u2076", 7: "\u2077", 8: "\u2078", 9: "\u2079", 10:"\u00B9\u2070"}
+6: "\u2076", 7: "\u2077", 8: "\u2078", 9: "\u2079"}
 for i in range((len(cof)-1), 1, (-1)):
     # if i<=9:
        arr.append('x'+utf[i]+"+")
     # if i>9:
     #     a=i/10
     #     b=i%10
-    #     arr.append('x'+utf[a]+"+")   
+    #     arr.append('x'+utf[a]+utf[b]"+")   
 arr.append('x+')
 arr.append('')
 print(arr)
@@ -32,9 +32,9 @@ print(arr_new)
 exs="".join(arr_new)
 print(exs)
 
-# data=open('file.txt', 'w')
-# data.write(exs)
-# data.close()
+data=open('file2.txt', 'w', encoding='utf-8')
+data.write(exs)
+data.close()
 
 
 
